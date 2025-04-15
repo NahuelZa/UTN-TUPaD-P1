10b25effea8d27f040d803dd923e5eb973abc8e8
Nahuel <nahuel_zabala@live.com>
2025-04-15 15:36:20 +0200

    Correcion Ej3

--- 05 Funciones/Ejercicio 3.py
+++ 05 Funciones/Ejercicio 3.py
@@ -1,13 +1,14 @@
 #Funciones
 
-def informacion_personal(nombre):
-    nombre=input("Ingrese nombre: ")
-    apellido=input("Ingrese apelldio: ")
-    edad=int(input("Ingrese edad: "))
-    residencia=input("Ingrese residencia: ")
+def informacion_personal(nombre,apellido,edad,residencia): 
     print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
 #Programa Principal
-informacion_personal()
+nombre=input("Ingrese nombre: ")
+apellido=input("Ingrese apelldio: ")
+edad=int(input("Ingrese edad: "))
+residencia=input("Ingrese residencia: ")
+
+informacion_personal(nombre,apellido,edad,residencia)
 
 
